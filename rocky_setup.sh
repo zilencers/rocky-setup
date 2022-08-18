62249d9c58f127c8f5a6f18f2699226fb58edf45
zilencers <91873611+zilencers@users.noreply.github.com>
2022-08-18 13:54:46 -0500

    various updates

--- rocky_setup.sh
+++ rocky_setup.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
-# Updating the system
-dnf -y update
-
-# Add EPEL Repository
-echo "\nAdding EPEL Repository..."
-if [ ! $(dnf repolist | grep -i epel) ] ; then
-    dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
-fi
-
-# Install Packages
-echo "\nInstalling Packages..."
 declare -A packages
 packages[git.x86_64]='-y'
 packages[cockpit.x86_64]='-y'
 packages[wpa_supplicant.x86_64]='-y'
 packages[dkms.noarch]='--enablerepo="epel"'
+packages[hostapd.x86_64]='-y'
+packages[snapd.x86_64]='-y'
+packages[vim-X11]='-y'
+packages[kernel-devel]='-y'
+packages[zfs]='-y'
+
+
+# Add Repositories
+echo "Adding EPEL Repository..."
+dnf install epel-release
+
+echo "\nAdding ZFS Repository..."
+dnf install https://zfsonlinux.org/epel/zfs-release-2-2$(rpm --eval "%{dist}").noarch.rpm
+gpg --import --import-options show-only /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
+
+# Updating the system
+dnf -y upgrade
+
+# Install Packages
+echo "\nInstalling Packages..."
 
 for key in "${!packages[@]}"; do  
     if [ ! $(dnf list installed | grep -i $key) ] ; then
@@ -23,6 +32,15 @@ for key in "${!packages[@]}"; do
     fi
 done
 
+# Enable SNAPD
+systemctl enable snapd
+
+# Start SNAPD
+systemctl start snapd
+
+# Install LXD
+echo "\nInstalling LXD..."
+snap install lxd
 
 # Install Wireless Drivers
 echo "\nInstalling Rtl88x2bu wireless drivers"
