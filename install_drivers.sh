84c10cefa8c62d76d37fbb1fb925b3b983e31736
zilencers <91873611+zilencers@users.noreply.github.com>
2022-08-19 13:12:24 -0500

    various updates

---
+++ install_drivers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+# Install Wireless Drivers
+echo "Installing Rtl88x2bu wireless drivers"
+git clone "https://github.com/RinCat/RTL88x2BU-Linux-Driver.git" /usr/src/rtl88x2bu-git
+sed -i 's/PACKAGE_VERSION="@PKGVER@"/PACKAGE_VERSION="git"/g' /usr/src/rtl88x2bu-git/dkms.conf
+sudo dkms add -m rtl88x2bu -v git
+sudo dkms autoinstall
+
